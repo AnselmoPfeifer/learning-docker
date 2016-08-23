ce1f309e21bf5920d8502045c0697f103340f723
Anselmo Pfeifer <anselmo.pfeifer@tecsinapse.com.br>
2016-08-23 08:45:56 -0400

    Add cenarios

---
+++ HighAvailability/exemplo02/start.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+echo "Recriando os 4 containers."
+docker rm $(docker ps -qa) -f
+
+echo "Criando os 3 containers com tomcat:8.5.4"
+cd tomcat
+cp /Users/anselmo/Documents/Desenvolvimento/workspace/DevOps/produtos/target/ROOT.war tomcat/
+docker build -t tomcat:8.5 .
+pwd
+sleep 2
+docker run -d --name host1 -p 8081:8080 -p 45564:45564 tomcat:8.5
+docker run -d --name host2 -p 8082:8080 -p 45565:45564 tomcat:8.5
+docker run -d --name host3 -p 8083:8080 -p 45566:45564 tomcat:8.5
+
+echo "Criando os 1 container de loadbalancer com nginx"
+cd ../loadbalancer/
+pwd
+sleep 2
+docker run -d --name loadbalancer -p 80:80 -p 443:443 --link host1:host1 --link host2:host2 --link host3:host3 --env-file ./env.list jasonwyatt/nginx-loadbalancer
+echo "Acesse http://localhost"
